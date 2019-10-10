99fe13741fc0082f694a8b46883b5bf5dea259e2
Hagar <Hagar_Usama@outlook.com>
2019-10-10 11:40:20 +0200

    exploring execv()

---
+++ exp_execv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  // perror()
+#include <stdlib.h> // exit(), EXIT_SUCCESS, EXIT_FAILURE , system()
+#include <unistd.h> // fork() , execvp()
+
+
+int main(){
+
+	exe
+	
+
+
+
+return 0;
+}
